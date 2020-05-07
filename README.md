6242e7b84bc24a7db32415d8f52d693cb3ff7939
Sean Wilkerson <seanldmcdonald@gmail.com>
2020-05-07 00:47:22 -0600

    Basic structure of core objects.

---
+++ README.md
@@ -0,0 +1,17 @@
+# graphics-library
+
+Super basic 3D graphics engine I'm writing as a pastime. Probably
+won't work correctly for quite a while.
+
+Not that anyone but me will care.
+
+## Roadmap
+
+- Implement shapes
+    - 2D Shapes
+        - Squares
+        - Circles
+    - 3D Shapes:
+        - Cubes
+        - Spheres
+- Color
---
+++ core.py
@@ -0,0 +1,92 @@
+# A very naive and basic project dedicated to making a 3D
+# rendering engine that displays to the screen.
+# |-> Written by Sean Wilkerson @ github.com/Seanld
+
+from math import sin, cos, radians
+import numpy
+
+
+
+class Screen:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.buffer = [[0] * width] * height
+
+    def paint(self, x, y, value):
+        self.buffer[y][x] = value
+
+
+
+class Camera:
+    # position: physical location of camera.
+    # screenDistance: distance of the screen from physical location of the camera.
+    def __init__(self, position=[-20, 0, 0], space=None, screenDistance=10, screenWidth=200, screenHeight=150):
+        self.screen = Screen(screenWidth, screenHeight)
+
+        if space == None:
+            self.space = Space()
+        else:
+            self.space = space
+
+        self.position = position
+        self.screenDistance = screenDistance
+
+    # Will iteratively call the render functions of all object instance currently in the space.
+    def render(self, spaceToRender):
+        for _object in spaceToRender.objects:
+            pass # Do rendering math here (maybe put rendering math for each object in its definition, and refer separately.)
+
+    # Absolute camera movement.
+    def moveTo(self, position):
+        self.position = position
+    
+    # Relative camera movement.
+    def moveBy(self, increment):
+        self.position[0] += increment[0]
+        self.position[1] += increment[1]
+        self.position[2] += increment[2]
+
+
+
+class Space:
+    def __init__(self):
+        self.objects = {}
+    
+    def addObject(self, id, _object):
+        self.objects[id] = _object
+    
+    def deleteObject(self, id):
+        self.objects.pop(id)
+
+    # aPos is starting location, bPos is destination/testing location.
+    def checkCollision(self, aPos, bPos):
+        pass
+
+
+    
+class Object:
+    def __init__(self, position=[0, 0], vertices=[]):
+        self.position = position
+        self.vertices = vertices
+
+    # Objects' vertices are relative to its position. To get absolute
+    # vertices in relation to the space, use this method.
+    def absoluteVertices(self):
+        _absVertices = []
+
+        for v in self.vertices:
+            _current = []
+
+            for i in v:
+                _current.append(self.position + i)
+
+            _absVertices.append(_current)
+        
+        return _absVertices
+
+
+
+class Square (Object):
+    def render(self):
+        pass
